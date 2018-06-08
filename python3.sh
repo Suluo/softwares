8a1d40824a0a9517fd86f0246014825049b986d7
suluo <sam_suluo@163.com>
2018-06-08 17:10:51 +0800

    feat(python2.sh python3.sh): init python2, python3 user
    用户自定义安装python2，python3

---
+++ python3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# -*- coding:utf-8 -*-
+############################################
+# File Name    : python3.sh
+# Created By   : Suluo - sampson.suluo@gmail.com
+# Creation Date: 2018-06-08
+# Last Modified: 2018-06-08 17:06:07
+# Descption    :
+############################################
+
+set -x
+
+script_path=/home/suluo
+
+mkdir -p /home/suluo/softwares
+cd $scrpit_path/softwares
+
+wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tgz
+
+tar -xzf Python-3.6.5.tgz
+cd Python-3.6.5
+mkdir -p $script_path/.local
+./configure --prefix="$script_path/.local"
+make && make install
